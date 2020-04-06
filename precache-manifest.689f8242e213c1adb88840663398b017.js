0ea35cf7ec74bb1181d3a33ee48c1a2252e89383
janvisharma <janvi@connect.hku.hk>
2020-04-06 14:27:27 +0530

    Updates

---
+++ precache-manifest.689f8242e213c1adb88840663398b017.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8982fddf4a62e64b0e166fa707f6bdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b7c78e90d3a9476a7b4",
+    "url": "/static/css/main.fdac441f.chunk.css"
+  },
+  {
+    "revision": "6e5460253561671c36ce",
+    "url": "/static/js/2.53ff6f9c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.53ff6f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7c78e90d3a9476a7b4",
+    "url": "/static/js/main.5db5cb6b.chunk.js"
+  },
+  {
+    "revision": "8b585dcdd11e488d9ca6",
+    "url": "/static/js/runtime-main.164c5f16.js"
+  },
+  {
+    "revision": "ec243b7e54262969f384d7cea259e32f",
+    "url": "/static/media/1.ec243b7e.jpg"
+  },
+  {
+    "revision": "83c070978ca8c25b115cc2b20c76c5d4",
+    "url": "/static/media/ClubLogo.83c07097.png"
+  },
+  {
+    "revision": "2ac1bb3cf3b484d345c1d1b6dca184d6",
+    "url": "/static/media/backgrounimg.2ac1bb3c.jpg"
+  },
+  {
+    "revision": "48200817cffeb3b7eecff08772b1b8eb",
+    "url": "/static/media/gela.48200817.jpg"
+  },
+  {
+    "revision": "99b5a126117f3f23acf946e5996a9678",
+    "url": "/static/media/janvi.99b5a126.jpg"
+  },
+  {
+    "revision": "e4797bb7396c3dceaac4bf8af4d08ba8",
+    "url": "/static/media/owl.e4797bb7.png"
+  },
+  {
+    "revision": "da7f7f45ca9763c7738defc8792637d8",
+    "url": "/static/media/seanchok.da7f7f45.jpg"
+  },
+  {
+    "revision": "42702f86f18cf6f4fe34aa9e27572e1c",
+    "url": "/static/media/siddhant.42702f86.jpg"
+  },
+  {
+    "revision": "000429b6a1f9a5f00673d648e9a52160",
+    "url": "/static/media/sls.000429b6.png"
+  },
+  {
+    "revision": "71c2bd6ee5ac657c88339244b0e8539f",
+    "url": "/static/media/welvin.71c2bd6e.jpg"
+  }
+]);
--- styles-e43938428353885ebca5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{q4sD:function(n,w,o){}}]);
-//# sourceMappingURL=styles-e43938428353885ebca5.js.map
